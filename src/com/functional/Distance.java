74c770ceb15368f5498946b9bc442e25956ae733
sreeliptaa <86236489+sreeliptaa@users.noreply.github.com>
2021-08-09 16:00:43 +0530

    calculating distance of two coordinates

--- src/com/functional/Distance.java
+++ src/com/functional/Distance.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Distance {
       // Function to calculate the value of distance
     static void calculate(int x, int y) {
-        System.out.println("Distance is " + Math.pow((x * x + y * y), 0.5));
+        double result = (Math.sqrt((Math.pow(x, 2)) + (Math.pow(y, 2))));
+		System.out.println("The Euclidean distance is: " + result)
     }
 
     public static void main(String[] args) {
         try {
             Scanner s = new Scanner(System.in);
-            System.out.println("Enter Value for x");
+            System.out.println("Enter Value for x coordinate");
             int x = s.nextInt();
-            System.out.println("Enter Value for y ");
+            System.out.println("Enter Value for y coordinate ");
             int y = s.nextInt();
             calculate(x, y);
             s.close();
